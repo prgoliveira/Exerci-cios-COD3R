86cf5accae918e4391ddd09372312f51e7a34afa
prgoliveira <prgoliveira@users.noreply.github.com>
2020-08-19 17:00:13 -0300

    Resolução do Exercicio 34 com Set e forOf
    Aplicando os conceitos de Set e for of aprendidos na seção de esnext para a resolução da atividade

--- Fundamentos/ex34.js
+++ Fundamentos/ex34.js
@@ -3,3 +3,30 @@ Construa uma função que receberá duas Strings de tamanhos variados e que reto
 todos os caracteres (independentemente de ser maiúsculo ou minúsculo) estejam contidos em ambas palavras.
 */
 
+const firstString = 'abcdefg aabbh1'
+const secondString = 'Abcdefg bh1'
+const thirdString = 'abcdefgbh1 1l'
+
+const stringCompare = (string1, string2) => {
+    
+    const stringOne = (string1.toLowerCase()).split('')
+    const stringTwo = (string2.toLowerCase()).split('')
+
+    const setOne = new Set(stringOne)
+    const setTwo = new Set(stringTwo)
+
+    function setCompare(set1, set2) {
+        if (set1.size !== set2.size) return false
+        //forOf percorre cada elemento do primeiro Set e verifica se aquele elemento esta presente no segundo Set
+        for (var set of set1){
+            if (!set2.has(set)) 
+                return false;
+        } 
+
+        return true;
+    }
+    return console.log(setCompare(setOne, setTwo))
+}
+
+stringCompare(firstString, secondString)
+stringCompare(firstString, thirdString)
