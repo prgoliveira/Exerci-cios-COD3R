401eb2a6edfd007b1c9d35ed03e3b551269b11ca
prgoliveira <prgoliveira@users.noreply.github.com>
2020-08-20 21:00:02 -0300

    Resolução do ex38

---
+++ Fundamentos/ex38.js
@@ -0,0 +1,17 @@
+/*
+Escreva uma função que receba dois parâmetros início e fim. Essa função deve imprimir todos os números
+ímpares que estão entre esses valores. Por padrão os valores devem ser 0 para início e 100 para fim. 
+Atente para corrigir a ordem dos parâmetros caso a função receba o valor maior antes do menor.
+*/
+const printImpar = (min, max) => {
+    if(min>max){
+        [min, max] = [max, min]
+    }
+    for(let i=min; i<=max; i++){
+        if(i%2!=0){
+            console.log(i)
+        }
+    }
+}
+
+printImpar(50, 20)
